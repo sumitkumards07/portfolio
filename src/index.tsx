0a4b2ee96baa1ddee43d8cbbc750a61cfdcd1ecf
sumitkumards07 <sumitkumards07@gmail.com>
2026-01-01 23:39:54 +0530

    feat: integrate Vercel Analytics

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import { Analytics } from "@vercel/analytics/react";
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <Router>
     <App />
+    <Analytics />
   </Router>
 );
 
